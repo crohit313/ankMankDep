bb9bfc28c27667a5ace7a854de48e2f7aa15a9f5
crohit313 <crohit313@gmail.com>
2017-12-27 20:49:22 +0530

    'check'

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -1,13 +1,13 @@
 // Init the application configuration module for AngularJS application
 var ApplicationConfiguration = (function () {
   // Init module configuration options
+  console.log('in config client app')
   var applicationModuleName = 'mean';
   var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ngSanitize','ui.bootstrap', 'ui.utils', 'angularFileUpload', 'ngMask', 'restangular', 'ui-notification', 'gm', 'ngFileUpload','ngTableToCsv'];
   // '720kb.datepicker'
   // Add a new vertical module
   var registerModule = function (moduleName, dependencies) {
     // Create angular module
-console.log(moduleName);
     angular.module(moduleName, dependencies || []);
 
     // Add the module to the AngularJS configuration file
--- modules/core/client/directives/sideNavbar.client.directive.js
+++ modules/core/client/directives/sideNavbar.client.directive.js
@@ -1,4 +1,5 @@
-angular.module('core').directive('sideNavBar', function($sce) {
+angular.module('core').directive('sideNavBar', function($scope) {
+    console.log('in directive')
     return {
         restrict: 'AEC',
         templateUrl: '/modules/core/views/sideNav.client.view.html',
